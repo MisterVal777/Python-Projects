cf8e1811d1b9d371225e3741d11466326717b5ee
MisterVal777 <MisterVal7@outlook.com>
2023-03-29 11:59:25 -0700

    Commit inheritance assignment example

---
+++ Python/InheritanceAssignment.py
@@ -0,0 +1,17 @@
+# class vehicle, all with ' ' are sting values.  Without ' ' are integer values.
+class Vehicle:
+	manufactuer = ' '
+	horsepower = ' '
+	fuel_type = ' '
+	saftey_rating = 
+
+# Truck inherits from Vehicle plus its own strong and integer values
+class Truck(Vehicle)
+	tow_rate = ' '
+	passenger_occupancy =
+	transmission = ' '
+
+# Motorcycle inherets from vehicle plus its own string values
+class Motorcycle(Vehicle):
+	seat_height = ' '
+	color = ' '
